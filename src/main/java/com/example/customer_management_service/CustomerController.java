ede8cf4e1f7155e30d575d521bbc6b9ab560d407
Shivani Kolanu <shivanikolanu2000@gmail.com>
2024-08-06 17:22:54 +0000

    Post and Delete APIs

--- src/main/java/com/example/customer_management_service/CustomerController.java
+++ src/main/java/com/example/customer_management_service/CustomerController.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -79,6 +81,16 @@ public class CustomerController {
                 return repo.save(newCustomer);
             });
 
+    }
+
+    @DeleteMapping("/customers/deleteCustomer/{id}")
+    public void deleteCustomerByID(@PathVariable int id){
+        repo.deleteById(id);
+    }
+
+    @PostMapping("/customers/insertCustomer")
+    public Customer newCustomer(@RequestBody Customer newCustomer){
+        return repo.save(newCustomer);
     }
 
     
