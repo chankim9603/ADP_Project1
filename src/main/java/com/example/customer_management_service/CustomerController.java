8f74b3fd7cc368da8761e80e2e75748c1ceb5cf1
Shivani Kolanu <shivanikolanu2000@gmail.com>
2024-08-06 15:48:37 +0000

    Update (put) api

--- src/main/java/com/example/customer_management_service/CustomerController.java
+++ src/main/java/com/example/customer_management_service/CustomerController.java
@@ -2,10 +2,14 @@ package com.example.customer_management_service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,27 +31,61 @@ public class CustomerController {
     public String welcome(){
         return "Welcome to the Customer Management!";
     }
-
+    // Get all customers.
     @GetMapping("/customers")
     public List<Customer> getAllCustomers(){
         return repo.findAll();
 
     }
-
+    // Get a customer by id.
     @GetMapping("/customers/{id}")
-    public Customer getCustomerbyID(@PathVariable("id") int id){
-        for(Customer customer: customers){
-            if(customer.getId() == id){
-                return customer;
-            }
-        }
-        return null;
-        
-        // return customers.get(id);
-
+    public Optional<Customer> getCustomerbyID(@PathVariable("id") int id){
+        return repo.findById(id);
+        // type mismatch, so changed to <optional>
+        // for(Customer customer: customers){
+        //     if(customer.getId() == id){
+        //         return customer;
+        //     }
+        // }
+        // return null;
         
     }
 
+    // Update a customer.
+
+    
+	// public ResponseEntity<?> updateCustomer(
+	// 		@RequestBody Customer newCustomer,
+	// 		@PathVariable("customerId") long customerId) 
+	// {
+	// 	if (newCustomer.getId() != customerId || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+	// 		return ResponseEntity.badRequest().build();
+	// 	}
+	// 	newCustomer = repo.save(newCustomer);
+	// 	return ResponseEntity.ok().build();
+	// }
+
+    @PutMapping("/{customerId}")
+    public Customer updateCustomer(@RequestBody Customer newCustomer, 
+    @PathVariable("customerId") int customerId){
+
+        return repo.findById(customerId)
+            .map(customer -> {
+                customer.setName(newCustomer.getName());
+                customer.setEmail(newCustomer.getEmail());
+                return repo.save(customer);
+            })
+            .orElseGet(() -> {
+                return repo.save(newCustomer);
+            });
+
+    }
+
+    
+
+
+
+    
 
 
 
